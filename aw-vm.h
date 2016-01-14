d9394e8f59b84d3d5e2cb3d87ef175667af09d71
afterwise <afterwise@gmail.com>
2016-01-14 16:05:48 +0100

    Example snippet of how to use vm and lma to create a growable, contiguous area.

--- aw-vm.h
+++ aw-vm.h
@@ -50,6 +50,35 @@ void vm_release(void *p, size_t n);
 void *vm_commit(void *p, size_t n, int flags);
 void vm_decommit(void *p, size_t n);
 
+/*
+   Example snippet of how to use vm and lma to create a growable, contiguous area.
+   Include aw-lma.h for this to compile.
+ */
+
+#define vlma_init(lma,n) do { \
+		_lma_assert(((n) & (vm_page - 1)) == 0); \
+		lma_init(lma, vm_reserve(n), n); \
+	} while (0)
+#define vlma_destroy(lma) do { \
+		vm_release((lma)->base, (lma)->end - (lma)->base); \
+	} while (0)
+#define vlma_alloc(lma,p,n) do { \
+		while ((*(p) = lma_alloc_low(lma, n)) == NULL) { \
+			if ((lma)->high >= (lma)->end) \
+				abort(); \
+			vm_commit((lma)->high, ((n) + (vm_page - 1)) & ~(vm_page - 1), 0); \
+			(lma)->high += ((n) + (vm_page - 1)) & ~(vm_page - 1); \
+		} \
+	} while (0)
+#define vlma_free(lma,p) do { \
+		_lma_assert((lma_addr_t) (p) >= (lma)->base); \
+		_lma_assert((lma_addr_t) (p) < (lma)->low); \
+		lma_addr_t _vlma_free_p = \
+			(lma_addr_t) (((uintptr_t) (lma)->low + (vm_page - 1)) & ~(vm_page - 1)); \
+		vm_decommit(_vlma_free_p, (lma)->high - _vlma_free_p); \
+		(lma)->high = _vlma_free_p; \
+	} while (0)
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
